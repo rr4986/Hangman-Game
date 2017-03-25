9ca2abf823efdcfa003ccba1d84204e38cb66a39
Ruben Reyes <ruben.r.reyes@gmail.com>
2017-03-25 00:00:32 -0500

    index created, working on the javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var winCounter = 0;
+var dashLetters;
+var lettersGuessed;
+var wordBank;
+
+wordBank = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto", "galaxy", "nebula", "spaceship", "sun", "asteroid", "comet"]
+
+var randomNumber=Math.floor(Math.random()*14+1);
+
+
+
+// selectedWord = wordBank[]
